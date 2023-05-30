3face453a1bfe228f0fa817f1146930884a1d70a
ueredeveloper <ueredeveloper@gmail.com>
2023-05-30 17:39:06 -0300

    iniciando a página login

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Califórnia Water Use
+# California Water Use
 
 Criar um aplicativo utilizando como banco de dados algo semelhante às necessidades do estado da Califórnia.
 
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, View, Text, Button, Image } from 'react-native';
+import Checkbox from 'expo-checkbox';
 
+const LoginForm = () => {
 
-const LoginForm = ()=>{
+    const [isChecked, setChecked] = useState(false);
+
     return (
-        <View>
-            <Text>California Water Use</Text>
-            <Text>Username</Text>
-            <TextInput/>
-            <Text>Password</Text>
-            <TextInput/>
+        <View style={{ display: 'flex', flex: 1 }}>
+            <View style={{ backgroundColor: 'blue', alignItems: 'center' }}>
+                <Image
+                    style={{ width: 100, height: 100 }}
+                    source={require('../assets/logo.png')} />
+            </View>
+            <View style={{ backgroundColor: 'orange', alignItems: 'center' }}>
+                <Text style={{ fontSize: 25 }}>California Water Use</Text>
+            </View>
+            <View style={{ backgroundColor: 'gray' }}>
+                <Text>Username</Text>
+                <TextInput />
+                <Text>Password</Text>
+                <TextInput />
+                <Checkbox
+                    style={{ margin: 10 }}
+                    value={isChecked}
+                    onValueChange={setChecked}
+                    color={isChecked ? '#4630EB' : undefined}
+                />
+
+                <Text >Remember me</Text>
+                <Text >Forgot Password?</Text>
+
+            </View>
+            <View style={{ backgroundColor: 'purple', height: 50 }}>
+                <Button
+                    title="Press me"
+                    onPress={() => console.warn('Simple Button pressed')}
+                />
+            </View>
+
         </View>
     )
 }
